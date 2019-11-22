081c2f08c0f0fa5240da79479de8a5042edc5748
Anton Miroshkin <anton.miroshkin@gmail.com>
2019-11-22 16:10:08 +0300

    Init project

---
+++ DesignPatterns_AbstractFactory/DesignPatterns_AbstractFactory/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DesignPatterns_AbstractFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("DesignPatterns_AbstractFactory!");
+        }
+    }
+}
